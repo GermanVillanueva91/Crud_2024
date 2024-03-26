7e2f9d864d82f292ae43e4019be4215164244de6
GermanVillanueva91 <xander_sn_91@hotmail.com>
2024-03-26 17:33:38 -0600

    Cargando todos los archivos del proyecto

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php
+include_once 'bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$consulta = "SELECT id, nombre, pais, edad FROM personas";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="#" />
+    <title>Libreria</title>
+ <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- CSS personalizado -->
+    <link rel="stylesheet" href="estilo.css">
+    <!--datables CSS básico-->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.min.css">
+    <!--datables estilo bootstrap 5 CSS-->
+    <link rel="stylesheet"  type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet"  type="text/css" href="https://cdn.datatables.net/2.0.2/css/dataTables.bootstrap5.css">       
+
+
+</head>
+<body>
+    <header >
+       
+        <h4 class="text-center p-3 mb-2 bg-success text-white">CRUD con DATATABLES</h4>
+    </header>
+
+    <div class="container">
+        <div class="row"></div>
+        <div class="col-lg-12">
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>
+        </div>
+        </div>
+    </div>
+<br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive">
+                    <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
+                        <thead class="text-center">
+                            <tr>
+                                <th>Id</th>
+                                <th>Nombre</th>
+                                <th>Pais</th>
+                                <th>Edad</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+
+
+
+                        <tbody>
+                            <?php
+                            foreach($data as $dat) {
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['nombre'] ?></td>
+                                <td><?php echo $dat['pais'] ?></td>
+                                <td><?php echo $dat['edad'] ?></td>
+                                <td></td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Modal para CRUD-->
+    <div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"></h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form id="formPersonas">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="nombre" class="col-form-label">Nombre:</label>
+                            <input type="text" class="form-control" id="nombre">
+                        </div>
+                        <div class="form-group">
+                            <label for="pais" class="col-form-label">País:</label>
+                            <input type="text" class="form-control" id="pais">
+                        </div>
+                        <div class="form-group">
+                            <label for="edad" class="col-form-label">Edad:</label>
+                            <input type="number" class="form-control" id="edad">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- datatables JS -->
+
+
+
+    <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+
+    <script type="text/javascript" src="main.js"></script>
+ 
+
+
+
+</body>
+
+</html>
